6b99af176619ab171af48c40e9833299d8052917
Farid <fkarimli047@gmail.com>
2024-05-12 15:07:37 +0400

    Package Structure is created related with the Booking part and bookTheFlight method is added.Database is only in Memory

---
+++ src/main/java/service/exceptions/DuplicateBookingEntryException.java
@@ -0,0 +1,10 @@
+package service.exceptions;
+
+
+
+public class DuplicateBookingEntryException extends RuntimeException{
+    public DuplicateBookingEntryException(String message) {
+        super(message);
+    }
+}
+
